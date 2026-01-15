0ff7758daf7641206c66831c2866a41213df3fdb
Dominic-SR <dominicseduraja3@gmail.com>
2026-01-15 16:02:17 +0530

    reducer setup done

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,11 +1,32 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ActionReducer, provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore, routerReducer } from '@ngrx/router-store';
+import {localStorageSync} from 'ngrx-store-localstorage';
+import { authReducer } from './store/auth/auth.reducer';
 
 import { routes } from './app.routes';
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({
+    keys : ['auth'],
+    rehydrate : true,
+  })(reducer);
+}
+
+const metaReducers = [localStorageSyncReducer];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideStore({
+      router: routerReducer
+    },{metaReducers}),
+    provideEffects([]),
+    provideStoreDevtools(),
+    provideRouterStore(),
   ]
 };
