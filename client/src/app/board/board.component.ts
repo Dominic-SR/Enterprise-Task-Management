5911e4f7a614239b4a826ee491e47d8d28ec0db3
Dominic-SR <dominicseduraja3@gmail.com>
2026-02-02 23:55:58 +0530

    drag and drop added

--- client/src/app/board/board.component.ts
+++ client/src/app/board/board.component.ts
@@ -80,9 +80,7 @@ export class BoardComponent {
       .subscribe({
         next:(data:any)=>{
           this.getAssignPersonsData= data.data;
-          console.log("111>>>>",data.data);
           this.assignees[task_id] = data.data;
-          console.log(">>>>",this.assignees);
          this.cdr.detectChanges();
         },
         error:(data:any)=>{
